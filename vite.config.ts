e263b660f8285980b2594086375fb1c2170d944f
Omid Web <omid.345@gmail.com>
2025-04-01 20:17:13 -0400

    Refactor deployment workflow and update index.html title for improved asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Add this line to make assets load correctly
   server: {
     port: 3000,
     open: true,
